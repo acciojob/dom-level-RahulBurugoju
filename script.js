16683f887930bef92e5c369a3d680be867b58a84
RahulBurugoju <rahul9399555@gmail.com>
2026-05-15 12:43:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+let el = document.getElementById('level')
+  let lvl =Array.from(el.parentNode.children).indexOf(el);
+
+setTimeut(()=>alert(`The level of the element is : ${lvl}`),2000)
+         
